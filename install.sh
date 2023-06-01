a66d598a3a42fceedff5bf2e9d7c1736654f2e20
Mikael Lindahl <mikael.lindahl.07@gmail.com>
2023-06-01 13:56:19 +0200

    updated code to be more simplified

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+#!/bin/bash
+
+# Source the environment variables from the .env file
+export $(cat .env | xargs)
+
+# Use envsubst to substitute environment variables
+echo "Create docker-compose.yml from sample.docker-compose.yml and .env"
+echo ""
+envsubst < sample.docker-compose.yml > docker-compose.yml
+
+echo "Stop and remove previous setup"
+echo ""
+docker-compose stop
+docker-compose rm
+
+echo "Install"
+echo ""
+docker-compose up -d
+
+echo "Done!"
+echo""
+echo "Installation competed of pgadmin version $PGADMIN_VERSION!"
+echo "To login use this credentials as user and password"
+echo ""
+echo "PGADMIN_DEFAULT_EMAIL: $PGADMIN_DEFAULT_EMAIL"
+echo "PGADMIN_DEFAULT_PASSWORD: $PGADMIN_DEFAULT_PASSWORD"
+echo ""
